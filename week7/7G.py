e960c797a71d0875c8f1ef1fa30cb14dcf6aa9fe
milliax <wei.sivir.pw>
2021-11-16 16:01:38 +0800

    update

--- week7/7G.py
+++ week7/7G.py
@@ -1,97 +1,56 @@
-"""# 輸入
-column,row = list(map(int,input().split()))
+def show_map(floor,m):
+    endl = m-1
+    print("map")
+    for i in range(len(floor)):
+        if floor[i]:
+            print("O",end=" ")
+        else:
+            print("X",end=" ")
+        #print(floor[i],end=" ")
+        if i == endl:
+            print()
+            endl += m
+
+# 輸入
+m,n = list(map(int,input().split()))
 x,y = list(map(int,input().split()))
 
-floor = [False for i in range(row*column)]
+# 有 m*n 格地板
+floor = [False for i in range(m*n)]
 
+#老鼠的移動路徑
 steps = input().split()
-
-print(floor)
 
 count = 0
 
 for e in steps:
-    print("x: {},y: {}".format(x,y))
-    
+    count += 1
+    floor[y * n + x] = True
+    #show_map(floor,n)
+    #print("d: {},x: {},y: {}".format(e,x,y))
+
     if not False in floor:
         break
-    count += 1
-
-    floor[x * column + y] = True
     #print(floor)
-
     if e == '1':
         # going up
         if y != 0:
             y -= 1
         continue
     elif e == '2':
-        if x != row -1:
-            x += 1
         # going right
+        if x != (n - 1):
+            x += 1
         continue
     elif e == '3':
         # going down
-        if y != column -1:
+        if y != (m - 1):
             y += 1
         continue
     elif e == '4':
         # going left
         if x != 0:
             x -= 1
-        continue
-
-print(count)"""
-
-m,n = list(map(int,input().split()))
-y,x = list(map(int,input().split()))
-
-floor = [False for i in range(m*n)]
-#print(floor)
-
-direction = input().split()
-count = 0
-
-"""
-   m
-   x ->
-n  y | 1 | 2 | 3 |
-   | | - | - | - |
-   ^ | 4 | 5 | 6 |
-     | 7 | 8 | 9 |
-
-"""
-
-for e in direction:
-    #print(floor)
-    if not False in floor:
-        break
-    count += 1
-    print("d: {},x: {},y: {}".format(e,x,y))
-    floor[y * m + x] = True
-
-
-    if e == '1':
-        # going up
-        if not y == 0:
-            y -= 1
-        continue
-    elif e == '2':
-        # going right
-        if not x == (m-1):
-            x += 1
-        continue
-    elif e == '3':
-        # going down
-        if not y == (n-1):
-            y += 1
-        
-        continue
-    elif e == '4':
-        # going left
-        if not x == 0:
-            x -= 1
-
         continue
 
 print(count)
