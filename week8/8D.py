d6290b7284d850ee7f8dfa67f5e51513dbaf201a
milliax <wei.sivir.pw>
2021-11-23 14:39:52 +0800

    I don' t know how to program

--- week8/8D.py
+++ week8/8D.py
@@ -1,13 +1,24 @@
 ans = set()
-lines = 0
 
+seted_items = set()
+
+first_time = True
 while True:
     try:
         new = input().split(",")
-        ans.add(e for e in new)
-        lines += 1
+        if first_time:
+            first_time = False
+            seted_items = set(new)
+        else:
+            seted_items = seted_items & set(new)
+
+        for e in new:
+            ans.add(e)
+        #seted = set(new)
+        #ans.add(e for e in new)
     except:
         break
 
-print(lines-1)
-print(list(ans))
+ans = sorted(list(ans))
+print(len(seted_items))
+print(ans)
